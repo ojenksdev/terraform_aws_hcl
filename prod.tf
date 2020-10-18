61b3abeaeb655bb2e79e5c1716641c295213058a
ojenksdev <ojenksdev@gmail.com>
2020-10-18 00:45:48 -0500

    added asg, removed eip and manual instances

--- prod.tf
+++ prod.tf
@@ -51,34 +51,39 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
-resource "aws_instance" "prod_web" {
-  count = 2
 
-  ami           = "ami-0708a0921e5eaf65d"
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = "ami-0708a0921e5eaf65d"
   instance_type = "t2.micro"
+}
 
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-  ]
+resource "aws_autoscaling_group" "prod_web" {
+  name = "terraform-oj-autoscaling"
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  desired_capacity    = 2
+  max_size            = 4
+  min_size            = 2
+ 
 
-  tags = {
-      "Terraform": "true"
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Terraform"
+    value               = "true"
+    propagate_at_launch = true
   }
 }
-resource "aws_eip_association" "prod_web" {
-  instance_id = aws_instance.prod_web.0.id
-  allocation_id = aws_eip.prod_web.id
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
 }
 
-resource "aws_eip" "prod_web" {  
-  tags     = {
-    "Terraform": "true"
-  }
-}
 
 resource "aws_elb" "prod_web" {
   name            = "prod-web-lb"
-  instances       = aws_instance.prod_web.*.id
   subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
   security_groups = [aws_security_group.prod_web.id]
 
