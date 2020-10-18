8e9142926164edcfaa300e5f8fe9522a5c27ced8
ojenksdev <ojenksdev@gmail.com>
2020-10-17 23:51:01 -0500

    load balancer for prod

--- prod.tf
+++ prod.tf
@@ -10,6 +10,20 @@ resource "aws_s3_bucket" "prod_tf_course" {
 
 resource "aws_default_vpc" "default" {}
 
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-east-1a"
+  tags = {
+    "Terraform": "true"
+  }    
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-east-1b"
+  tags = {
+    "Terraform": "true"
+  }    
+}
+
 resource "aws_security_group" "prod_web" {
   name        = "prod_web"
   description = "allow standard http and https ports inbound and everything outbound"
@@ -38,6 +52,8 @@ resource "aws_security_group" "prod_web" {
 }
 
 resource "aws_instance" "prod_web" {
+  count = 2
+
   ami           = "ami-0708a0921e5eaf65d"
   instance_type = "t2.micro"
 
@@ -49,12 +65,30 @@ resource "aws_instance" "prod_web" {
       "Terraform": "true"
   }
 }
+resource "aws_eip_association" "prod_web" {
+  instance_id = aws_instance.prod_web.0.id
+  allocation_id = aws_eip.prod_web.id
+}
 
-resource "aws_eip" "prod_web" {
-  instance = aws_instance.prod_web.id
-
+resource "aws_eip" "prod_web" {  
   tags     = {
     "Terraform": "true"
   }
 }
 
+resource "aws_elb" "prod_web" {
+  name            = "prod-web-lb"
+  instances       = aws_instance.prod_web.*.id
+  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+      instance_port     = 80
+      instance_protocol = "http"
+      lb_port           = 80
+      lb_protocol       = "http"
+  }
+  tags = {
+    "Terraform": "true"
+  }
+}
